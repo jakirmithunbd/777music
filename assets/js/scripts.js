fb1680be7ef9c562b8da936c9e86ae48715ccf38
jakirmithunbd@gmail <jakirmithunbd@gmail.com>
2020-11-01 23:38:47 +0600

    working on the home page

---
+++ assets/js/scripts.js
@@ -0,0 +1,76 @@
+
+(function($){
+    var ua = window.navigator.userAgent;
+    var isIE = /MSIE|Trident/.test(ua);
+
+    if ( !isIE ) {
+        //IE specific code goes here
+        "use strict";
+    }
+
+      /*** Sidr Menu */
+    $('.navbar-toggle').sidr({
+        name: 'sidr-main',
+        side: 'right',
+        source: '#sidr',
+        displace: false,
+        renaming: false
+    });
+
+    $("document").on("click",function(e) { $.sidr('close','sidr-main'); });
+
+    $('.closeMenu').on('click', function(){
+        $.sidr('close', 'sidr-main');
+    });
+
+    /*** Sticky header */
+    // $(window).scroll(function() {
+
+    //     if ($(window).scrollTop() > 0) {
+    //       $(".header").addClass("sticky");
+    //     } 
+    //     else {
+    //       $(".header").removeClass("sticky");
+    //     }
+    // });
+
+    $('.toggle-menu-bar').on('click', function(){
+        $('.left-side-menu').toggleClass('show_left_sidebar');
+    });
+
+     // Mobile package Silder
+    $(".sliders").slick({
+        // dots: true,
+        infinite: true,
+        draggable: true,
+        slidesToShow: 1,
+        autoplay: true,
+        slidesToScroll: 1,
+        arrows: true,
+        responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            dots: true,
+            arrows: false,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+
+     // Banner Silder
+    $(".banner-sliders").slick({
+        dots: true,
+        infinite: true,
+        draggable: true,
+        slidesToShow: 1,
+        autoplay: true,
+        slidesToScroll: 1,
+        arrows: false,
+    });
+
+
+    
+})(jQuery);
